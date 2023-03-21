3353ad3f5b76fc27de80fa788c1476e3674c0c7d
mohamed El-Raghy <elraghy8@gmail.com>
2023-03-21 23:45:55 +0200

    parsing icoming requests

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('in the middleware function...');
-  next(); // allow the request to continue to the next middleware  in line
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+
+  res.send('<form action="/product" method="POST" ><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
-app.use((req, res, next) => {
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
 
-  console.log('in anthor middleware function...');
+
+app.use('/', (req, res, next) => {
   res.send('<h1>Hello from express!</h1>');
 
 });
