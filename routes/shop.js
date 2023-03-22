2c987e2fd4d7e55f68eff8b0dd6c5fddca978fe6
mohamed El-Raghy <elraghy8@gmail.com>
2023-03-22 23:27:45 +0200

    cofiger engine template

--- routes/shop.js
+++ routes/shop.js
@@ -10,8 +10,9 @@ const router = express.Router();
 
 
 router.get('/', (req, res, next) => {
-  console.log(adminData.products);
-  res.sendFile(path.join(rootDir, 'views', 'shop.html')); 
+  const products = adminData.products;
+
+  res.render('shop', { prods: products, docTitle: 'My Shop' });
 });
 
 module.exports = router;
