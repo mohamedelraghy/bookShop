3270eab351292c4c7d7ee5690a73ebc513f1b1fd
mohamed El-Raghy <elraghy8@gmail.com>
2023-03-23 00:02:27 +0200

    finshing templeting

--- routes/shop.js
+++ routes/shop.js
@@ -12,7 +12,7 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
   const products = adminData.products;
 
-  res.render('shop', { prods: products, docTitle: 'My Shop' });
+  res.render('shop', { prods: products, docTitle: 'My Shop', path: '/' });
 });
 
 module.exports = router;
