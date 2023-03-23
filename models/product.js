2d2772d712a3f4ad95ae70bb4f0948506d7f6574
mohamed El-Raghy <elraghy8@gmail.com>
2023-03-23 16:42:38 +0200

    storing Data in files

--- models/product.js
+++ models/product.js
@@ -1,4 +1,8 @@
-const products = []
+const { json } = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+const rootDir = require('../util/path');
+
 
 module.exports = class Product {
   constructor(t) {
@@ -6,10 +10,24 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    const p = path.join(rootDir, 'data', 'products.json');
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if(!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this); 
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
   }
 
   static fetchAll(){
-    return products;
+    const p = path.join(rootDir, 'data', 'products.json');
+    fs.readFile(p, (err, fileContent) => {
+      if(err) return []
+      return JSON.parse(fileContent);
+    })
   }
 }
