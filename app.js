b723ca078897ac144bac0af363d25323238e23f6
mohamed El-Raghy <elraghy8@gmail.com>
2023-03-23 03:16:34 +0200

    fix page not found typo

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-
 const app = express();
-
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -20,7 +18,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found', layout: false });
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path: "" });
 });
 
 app.listen(3000);
