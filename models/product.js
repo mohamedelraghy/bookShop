9590c70f953074be291dbe71691c21472e9cdfde
mohamed El-Raghy <elraghy8@gmail.com>
2023-03-23 17:09:13 +0200

    hh

--- models/product.js
+++ models/product.js
@@ -1,33 +1,33 @@
-const { json } = require('body-parser');
+
 const fs = require('fs');
 const path = require('path');
+
 const rootDir = require('../util/path');
 
+const p = path.join(rootDir, 'data', 'products.json');
+
+const getProductsFromFile = cb => {
+    fs.readFile(p, (err, fileContent) => {
+      if(err) return cb([])
+      cb(JSON.parse(fileContent));
+    });
+}
 
 module.exports = class Product {
   constructor(t) {
     this.title = t;
   }
 
-  save() {
-    const p = path.join(rootDir, 'data', 'products.json');
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if(!err) {
-        products = JSON.parse(fileContent);
-      }
-      products.push(this); 
+  save() { 
+    getProductsFromFile(products => {
+      products.push(this);
       fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
       });
     });
   }
 
-  static fetchAll(){
-    const p = path.join(rootDir, 'data', 'products.json');
-    fs.readFile(p, (err, fileContent) => {
-      if(err) return []
-      return JSON.parse(fileContent);
-    })
+  static fetchAll(cb){
+    getProductsFromFile(cb);
   }
 }
