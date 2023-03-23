9871820d29939a67fa297d5102234f363e67884d
mohamed El-Raghy <elraghy8@gmail.com>
2023-03-23 02:42:23 +0200

    add ejs as a templeting engine

--- app.js
+++ app.js
@@ -2,25 +2,25 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
- 
-const adminDate = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+
 
 const app = express();
 
-app.engine('handlebars', expressHbs());
-app.set('view engine', 'handlebars');
+
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.use(bodyParser.urlencoded({extended: false}));
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminDate.routes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: "Page Not Found"});
+  res.status(404).render('404', { pageTitle: 'Page Not Found', layout: false });
 });
 
 app.listen(3000);
