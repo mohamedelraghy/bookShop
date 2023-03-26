3596cc0a00e8214837967961fb9ebe49574d5169
mohamed El-Raghy <elraghy8@gmail.com>
2023-03-26 02:52:06 +0200

    add dummy user

--- app.js
+++ app.js
@@ -20,6 +20,17 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  User.findPk(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
@@ -28,9 +39,22 @@ app.use(errorController.get404);
 Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
 User.hasMany(Product);
 
-sequelize.sync({ force: true })
+sequelize
+  .sync()
+//.sync({ force: true })
   .then(result => {
+    return User.findByPk(1);
     // console.log(result);
+   
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'Mohamed', email:'test@test.com'})
+    }
+    return user;
+  })
+  .then(user => {
+    // console.log(user);
     app.listen(3000);
   })
   .catch(err => {
