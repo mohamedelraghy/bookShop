001b9294663919609c30e132f11f4a1b9b31c03a
mohamed El-Raghy <elraghy8@gmail.com>
2023-03-27 00:00:36 +0200

    adding new product to cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -67,33 +67,36 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  let fetchedCard;
+  let fetchedCart;
+  let newQuantity = 1;
   req.user
-    .getCard()
-      .then(cart => {
-        fetchedCart = cart; 
-        return cart.getProduct({where: {id: prodId}});
-      })
-      .then(products => {
-        let product;
-        if (products.lenght > 0) 
-          product = products[0];
-        let newQuantity = 1;
-        if (product) {
+    .getCart()
+    .then(cart => {
+      fetchedCart = cart;
+      return cart.getProducts({ where: { id: prodId } });
+    })
+    .then(products => {
+      let product;
+      if (products.length > 0) {
+        product = products[0];
+      }
 
-        }
-        return Product.findbyPk(prodId)
-          .then(product => {
-            return fetchedCart.addProduct(product, {
-              through: { quantity: newQuantity}
-            });
-          })
-          .catch(err => console.log(err));
-        })
-      .then(() => {
-          res.redirect('/cart');
-        })
-      .catch(err => console.log(err));
+      if (product) {
+        const oldQuantity = product.cartItem.quantity;
+        newQuantity = oldQuantity + 1;
+        return product;
+      }
+      return Product.findByPk(prodId);
+    })
+    .then(product => {
+      return fetchedCart.addProduct(product, {
+        through: { quantity: newQuantity }
+      });
+    })
+    .then(() => {
+      res.redirect('/cart');
+    })
+    .catch(err => console.log(err));
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
