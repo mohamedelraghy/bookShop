38ff416c812dbddb38941e5952ebeecb7503ba39
mohamed El-Raghy <elraghy8@gmail.com>
2023-03-28 23:16:15 +0200

    setting up MongoDB & comment sql

--- util/database.js
+++ util/database.js
@@ -1,8 +1,24 @@
-const Sequelize = require('sequelize');
+// const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('shop', 'root', 'password', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+// const sequelize = new Sequelize('shop', 'root', 'password', {
+//   dialect: 'mysql',
+//   host: 'localhost'
+// });
 
-module.exports = sequelize;
+// module.exports = sequelize;
+
+
+const mongodb = require('mongodb');
+const MongoClinet = mongodb.MongoClient;
+
+const mongoConnect = callback => {
+
+  MongoClinet.connect('mongodb+srv://Mohamed:Anaconda1@shop.zye25w7.mongodb.net/?retryWrites=true&w=majority')
+    .then(client => {
+      console.log('DB Connected');
+      callback(client);
+    })
+    .catch( err => console.log(err));
+}
+
+module.exports = mongoConnect;
