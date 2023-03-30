d77ce557e3035dbc13ecd608c6ad854d94ba60e8
mohamed El-Raghy <elraghy8@gmail.com>
2023-03-31 01:45:53 +0200

    getting orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Order = require('../models/order');
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -80,9 +81,21 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
-  let fetchedCart;
   req.user
-    .addOrder()
+    .populate('cart.items.productId')
+    .then(user => {
+      const products = user.cart.items.map(i => {
+        return {quantity: i.quantity, product: i.product};
+      });
+      const order = new Order({
+        user: {
+          name: req.user.name,
+          userId: req.user
+        },
+        products: products 
+      });
+      return order.save();
+    })
     .then(result => {
       res.redirect('/orders');
     })
@@ -91,8 +104,10 @@ exports.postOrder = (req, res, next) => {
 
 exports.getOrders = (req, res, next) => {
   req.user
-    .getOrders()
-    .then(orders => {
+    .populate('cart.items.productId')
+    .then(user => {
+      const orders = user.cart.items;
+      console.log(orders);
       res.render('shop/orders', {
         path: '/orders',
         pageTitle: 'Your Orders',
