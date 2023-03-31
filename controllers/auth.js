f6278f8f737185bad3249fa0211eb81c55681d1e
mohamed El-Raghy <elraghy8@gmail.com>
2023-03-31 23:24:22 +0200

    setting a cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,15 @@
 exports.getLogin = (req, res, next) => {
+  const isLoggedIn = req.get('Cookie').trim().split('=')[1];
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: req.isLoggedIn
+    isAuthenticated: isLoggedIn
   });
 };
 
 
 
 exports.postLogin = (req, res, next) => {
-  req.isLoggedIn = true;
+  res.setHeader('Set-Cookie', 'loggedIn=true');
   res.redirect('/');
 };
