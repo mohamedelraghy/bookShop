e7a091d3f6182f6eb959170b6c8918abc98a9475
mohamed El-Raghy <elraghy8@gmail.com>
2023-04-01 02:13:50 +0200

    deleting a cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
  console.log(req.session.isLoggedIn);
   res.render('auth/login', {
@@ -11,6 +13,20 @@ exports.getLogin = (req, res, next) => {
 
 exports.postLogin = (req, res, next) => {
   // res.setHeader('Set-Cookie', 'loggedIn=true'); // ? setting a cookie
-  req.session.isLoggedIn = true;                   // ? setting a session
-  res.redirect('/');
+  User.findById('6425fa2a8106168b66720a92')
+    .then(user => {
+      req.session.isLoggedIn = true;                   // ? setting a session
+      req.session.user = user;
+      res.redirect('/');
+    })
+    .catch(err => console.log(err));
+
 };
+
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
+  });
+}
