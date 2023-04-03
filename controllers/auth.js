ed56cf42132bed0d740c213570df45d0a8e4486a
mohamed El-Raghy <elraghy8@gmail.com>
2023-04-03 14:37:23 +0200

    update password

--- controllers/auth.js
+++ controllers/auth.js
@@ -157,7 +157,7 @@ exports.postReset = (req, res, next) => {
         return user.save();
       })
       .then(result => {
-        res.redirect('/');w
+        res.redirect('/');
         transporter.sendMail({
           to: req.body.email,
           from: 'elraghy8@gmail.com',
@@ -190,10 +190,35 @@ exports.getNewPassword = (req, res, next) => {
         pageTitle: 'Update Password',
         errorMessage: message,
         userId: user._id.toString(),
+        token: token
       });
     })
     .catch(err => console.log(err));
 }
 
 exports.postNewPassword = (req, res, next) => {
+  const newPassword = req.body.password;
+  const userId = req.body.userId;
+  const token = req.body.token;
+  let resetUser;
+
+  User.findOne({
+    resetToken: token, 
+    resetTokenExpiration: {$gt: Date.now()}, 
+    _id: userId 
+  })
+    .then(user => {
+      resetUser = user;
+      return bcrypt.hash(newPassword, 12);
+    })
+    .then(hashedPassword => {
+      resetUser.password = hashedPassword;
+      resetUser.resetToken = undefined;
+      resetUser.resetTokenExpiration = undefined;
+      return resetUser.save();
+    })
+    .then(result => {
+      res.redirect('/login');
+    })
+    .catch(err => console.log(err));
 }
