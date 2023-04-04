6236cf2122f6858bc7fb16d7291ec0c7556a0cd3
mohamed El-Raghy <elraghy8@gmail.com>
2023-04-04 23:21:49 +0200

    Adding conditinal css classes

--- controllers/auth.js
+++ controllers/auth.js
@@ -45,7 +45,8 @@ exports.getSignup = (req, res, next) => {
       email: "",
       password: "",
       confirmPassword: ""
-    }
+    },
+    validationErrors: []
   });
 };
 
@@ -103,7 +104,8 @@ exports.postSignup = (req, res, next) => {
       path: '/signup',
       pageTitle: 'Signup',
       errorMessage: errors.array()[0].msg,
-      oldInput: { email: email, password: password, confirmPassword: req.body.confirmPassword }
+      oldInput: { email: email, password: password, confirmPassword: req.body.confirmPassword },
+      validationErrors: errors.array()
     });
   }
 
