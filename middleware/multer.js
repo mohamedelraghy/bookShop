8927c3d81f139529b656084c264f61e155f4a117
mohamed El-Raghy <elraghy8@gmail.com>
2023-04-10 16:40:01 +0200

    add multer middleware

---
+++ middleware/multer.js
@@ -0,0 +1,30 @@
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        ;
+        cb(null, Date.now().toString() + '-' + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    const fileTypes = /jpeg|jpg|png/;
+    const mimeType = fileTypes.test(file.mimetype);
+
+
+    if (mimeType) return cb(null, true);
+
+    cb("Error: File upload only supports the "
+        + "following filetypes - " + fileTypes);
+}
+
+const upload = multer({
+
+    storage: storage,
+    fileFilter: fileFilter
+});
+
+module.exports = upload;
